0ad25a7b642febdf967b4505f3dc67d6ada8c18f
Enes Emre AYDOGAN <eeay0@protonmail.com>
2024-08-22 10:15:42 +0300

    error fixed

--- lua/eeay/core/lazy.lua
+++ lua/eeay/core/lazy.lua
@@ -16,7 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     spec = {
-        { import = "plugins" },
+        { import = "eeay.plugins" },
     },
     defaults = {
         lazy = true,
